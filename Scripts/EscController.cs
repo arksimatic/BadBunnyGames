6a492c0aa00ed2d20f46ee36c930a4b1e2896bd9
astisk <pobierac@gmail.com>
2024-07-27 18:28:29 +0200

    Modification of pause menu. Better UI design and fully functioning buttons for back/restart/menu

--- Scripts/EscController.cs
+++ Scripts/EscController.cs
@@ -2,14 +2,45 @@ using Godot;
 
 namespace BadBunnyGames
 {
-	public partial class EscController : Node2D
+	public partial class EscController : CanvasLayer
 	{
+		public override void _Ready()
+		{
+			ProcessMode = Node.ProcessModeEnum.Always;
+		}
 		public override void _Process(double delta)
 		{
-			if (Input.IsActionPressed(KeyCode.Esc))
+			if (Input.IsActionJustPressed(KeyCode.Esc))
 			{
-				GetChild<Panel>(0).Visible = true;
+				if(GetTree().Paused)
+				{
+					GetTree().Paused = false;
+					GetChild<PanelContainer>(0).Hide();
+				}
+				else
+				{
+					GetTree().Paused = true;
+					GetChild<PanelContainer>(0).Show();
+				}
 			}
+		}
+
+		private void _on_back_pressed()
+		{
+			GetTree().Paused = false;
+			GetChild<PanelContainer>(0).Hide();
+		}
+
+		private void _on_restart_pressed()
+		{
+			GetTree().Paused = false;
+			GetTree().ReloadCurrentScene();
+		}
+
+		private void _on_menu_pressed()
+		{
+			GetTree().Paused = false;
+			GetTree().ChangeSceneToFile($"{GlobalPaths.MainMenu}/Scenes/main_scene.tscn");
 		}
 	}
 }
