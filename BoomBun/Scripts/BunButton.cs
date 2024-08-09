b1cd00edb0694f6fd6265ffe71c9d291c78a4cea
astisk <pobierac@gmail.com>
2024-08-10 01:51:12 +0200

    BoomBun - Get textures via reference instead of ResourceLoader

--- BoomBun/Scripts/BunButton.cs
+++ BoomBun/Scripts/BunButton.cs
@@ -12,19 +12,14 @@ namespace BadBunnyGames.BomBun
 		public Boolean IsLocked { get; set; } = false;
 		public Int32 BunAroundCount { get; set; }
 		public Int32 Index(Int32 columns) => Y * columns + X;
-		private Texture2D _bombTexture;
-		private Texture2D _emptyTexture;
-		private Texture2D _coveredTexture;
-		private Texture2D _lockedTexture;
+		[Export] public Texture2D BombTexture;
+		[Export] public Texture2D EmptyTexture;
+		[Export] public Texture2D CoveredTexture;
+		[Export] public Texture2D LockedTexture;
 		private GameController _board { get; set; }
 		public override void _Ready()
 		{
-			_bombTexture = (Texture2D)ResourceLoader.Load($"{GlobalPaths.BoomBun}/Graphics/bomb.png");
-			_emptyTexture = (Texture2D)ResourceLoader.Load($"{GlobalPaths.BoomBun}/Graphics/empty.png");
-			_coveredTexture = (Texture2D)ResourceLoader.Load($"{GlobalPaths.BoomBun}/Graphics/unknown.png");
-			_lockedTexture = (Texture2D)ResourceLoader.Load($"{GlobalPaths.BoomBun}/Graphics/lock.png");
-
-			this.Icon = _coveredTexture;
+			this.Icon = CoveredTexture;
 			_board = GetParent<GameController>();
 		}
 		private void _on_pressed()
@@ -45,11 +40,11 @@ namespace BadBunnyGames.BomBun
 
 			if (IsBun)
 			{
-				this.Icon = _bombTexture;
+				this.Icon = BombTexture;
 			}
 			else
 			{
-				this.Icon = _emptyTexture;
+				this.Icon = EmptyTexture;
 				if (BunAroundCount > 0)
 					this.GetChild<Label>(0).Text = BunAroundCount.ToString();
 			}
@@ -64,9 +59,9 @@ namespace BadBunnyGames.BomBun
 			{
 				IsLocked = !IsLocked;
 				if (IsLocked)
-					this.Icon = _lockedTexture;
+					this.Icon = LockedTexture;
 				else
-					this.Icon = _coveredTexture;
+					this.Icon = CoveredTexture;
 			}
 		}
 
