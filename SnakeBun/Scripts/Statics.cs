fda0d0220482f01cf2b1d6033738654eae9abc6e
astisk <pobierac@gmail.com>
2024-08-22 14:45:40 +0200

    Cleaning

--- SnakeBun/Scripts/Statics.cs
+++ SnakeBun/Scripts/Statics.cs
@@ -6,7 +6,6 @@ namespace BadBunnyGames.SnakeBun
 	{
 		public static Single Speed = 2;
 		public static Int32 TileSize = 16;
-		//public static Vector2 MapStart = new Vector2(0, 0);
 		public static Int32 MapWidth = 10;
 		public static Int32 MapHeight = 10;
 	}
